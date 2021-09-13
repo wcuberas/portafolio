5e7dc505ae2693bd3508feecb8fbeec18b57d1de
wcuberas <wcuberas@gmail.com>
2021-09-13 10:40:45 -0300

    calculator app

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -32,6 +32,16 @@ class Projects extends Component {
         "https://booksquizapp-2f4fa.web.app/"
       );
     };
+    this.calculator = () => {
+      window.location.assign(
+        "https://github.com/wcuberas/react-calculator"
+      );
+    };
+    // this.appCalculator = () => {
+    //   window.location.assign(
+    //     "https://booksquizapp-2f4fa.web.app/"
+    //   );
+    // };
     this.movieFinder = () => {
       window.location.assign(
         "https://github.com/wcuberas/movie-finder"
@@ -125,6 +135,37 @@ class Projects extends Component {
                 <IconButton name="share" />
               </CardMenu>
             </Card> */}
+            {/* Calculator */}
+            <Card shadow={5} className="project-item">
+              <CardTitle
+                style={{
+                  color: "#333",
+                  height: "180px",
+                  background: "url(images/calculator.png) center / cover",
+                }}
+              >
+              </CardTitle>
+              <CardText style={{ textAlign: "justify", fontSize: '1.5rem' }}>
+                Calculator 
+              </CardText>
+              <CardActions border>
+                <Button
+                  style={{ margin: "0 10px" }}
+                  raised
+                  ripple
+                  primary
+                  onClick={this.calculator.bind(this)}
+                >
+                  <i className="fa fa-github" aria-hidden="true" /> Github
+                </Button>
+                <Button raised accent ripple style={{ margin: "0 10px" }}>
+                  <i className="fa fa-desktop" aria-hidden="true" /> Live Demo
+                </Button>
+              </CardActions>
+              <CardMenu style={{ color: "#fff" }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
             {/* Movie Finder */}
             <Card shadow={5} className="project-item">
               <CardTitle
